1c00c299f531d714bead260162fde161d78e11c1
Lisaselma <lisaselmavo@gmail.com>
2026-03-10 13:15:28 +0100

    changes to pipeline

--- big_data_assignment/pipeline/1_cleaning.py
+++ big_data_assignment/pipeline/1_cleaning.py
@@ -1,4 +1,10 @@
 """
 This script is used to clean the data.
 
+1. Convert disguised missing tokens to NaN.
+2. Assert accurate data types
+3. String & Categorical Standardization
+4. Outlier detection & treatment
+5. Numeric & Categorical Checks
+
 """
---
+++ big_data_assignment/pipeline/3_impute.py
@@ -0,0 +1,4 @@
+"""
+Uses MICE imputation to fill in missing values.
+Works well on numerical data.
+"""
