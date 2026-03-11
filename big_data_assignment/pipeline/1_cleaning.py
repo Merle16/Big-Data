9316d9b2d85e207c72b3cd926d4a7ecd3297ca48
Lisaselma <lisaselmavo@gmail.com>
2026-03-11 11:46:07 +0100

    pipeline

--- big_data_assignment/pipeline/1_cleaning.py
+++ big_data_assignment/pipeline/1_cleaning.py
@@ -1,10 +1,29 @@
 """
-This script is used to clean the data.
+This script is used to clean the data using DuckDB.
+For all, use DuckDB.
 
-1. Convert disguised missing tokens to NaN.
-2. Assert accurate data types
-3. String & Categorical Standardization
-4. Outlier detection & treatment
-5. Numeric & Categorical Checks
+Clean both data/raw/csv/* and data/raw/IMDB_external_csv/*
+
+1. Missingness detection: Convert disguised missing tokens to NaN with entropy detection.
+-> lisa/vanshita
+
+2. Assert accurate data types (ensure numeric columns are numeric, etc.).
+-> merle
+
+3. String & Categorical Standardization -> NFKD Unicode normalization
+-> lisa/ilesh
+
+4. Remove duplicate rows after normalization based on UUID. 
+Movies with the same name but different start years are always different movies.
+-> lisa/vanshita
+
+5. Outlier detection & treatment (IQR capping) (ask ilesh)
+-> ????
+
+6. MICE imputation for missing values.
+-> lisa
+
+7. safe all cleaned data to data/processed/csv/
+--> now it will be ready for feature engineering
 
 """
